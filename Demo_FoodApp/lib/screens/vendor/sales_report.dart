6754dd0a8a81a6862e7020180948fe76a2360e8b
drishyasmenon10 <drishyasmenon10@gmail.com>
2026-05-08 14:42:31 +0530

    Update sales_report.dart

--- Demo_FoodApp/lib/screens/vendor/sales_report.dart
+++ Demo_FoodApp/lib/screens/vendor/sales_report.dart
@@ -1,4 +1,4 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import '../../services/api_service.dart';
